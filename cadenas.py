8542866ff1b9e0fbe226e3c0465d70ba28b06820
511 <511@academicos.cesde.edu.co>
2023-07-28 09:25:33 -0500

    Cambio en cadenas

--- cadenas.py
+++ cadenas.py
@@ -19,7 +19,7 @@ txt="""
  como texto de relleno en documentos electrónicos, quedando esencialmente 
  igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset",
  las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de .
- autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem 
+ autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem
  Ipsum.
 """
 print(txt)
@@ -112,7 +112,80 @@ print("Las , está presente en el texto", x ,"veces")
 
 #6.
 
+print("SEXTO PUNTO")
 
+lrm=txt.count("Lorem")
+for texto in txt:
+    if "Lorem" == 0:
+        print("La parabra Lorem no existe")
+        break
+    else:
+        print("Hay", lrm ,"Lorem")
+        break
+
+
+#7.
+
+print("SEPTIMO PUNTO")
+
+hl=txt.count("hola")
+for texto in txt:
+    if hl == 0:
+        print("La parabra hola no existe")
+        break
+    else:
+        print("Hay", hl ,"hola")
+        break
+
+## CORTAR CADENAS
+#Rango
+print(txt[:100]) ## no incluye el 100 solo va del 0 al 99
+print("-------------------------------------------")
+print(txt[100:])
+
+## Negativa - empieza desde el final
+b = " Hola, mundo!"
+print(b[:-1])
+
+## realizar un ejercio basico con el random() float y vamos a cortar el numero 1:3,inicio:6 y del fin:-4
+## cadena sacar la longitud, inicio hasta longitud 
+## Upper case
+## Lower case
+
+import random
+
+numeroAleatorio = str(random.random())
+corte1 = numeroAleatorio[1:3]
+numeroInicio = numeroAleatorio[:6]
+numeroFinal = numeroAleatorio[:-4]
+print(numeroAleatorio)
+print("Este es el primer corte: ",corte1)
+print("Este es Inicio del segundo corte en 6: ",numeroInicio)
+print("Este es el fin del segundo corte -4: ",numeroFinal)
+
+print("-----------------------------------------")
+
+cadena = "Esta cadena es para el ejemplo del ejercicio"
+longitudCadena = len(cadena)
+print("La longitud de la cadena es: ",longitudCadena)
+
+subcadena = cadena[:longitudCadena]
+print(subcadena)
+
+
+print("-----------------------------")
+
+
+# format # maria camila
+nombre ="Juan"
+edad = 30
+cadenaFormateada = "Hola, soy {} y tengo {} años.".format(nombre,edad)
+print(cadenaFormateada)
+
+nombre1 = "Camila"
+aleatorio = str(random.random())
+cadenaFormat = "Hola soy {} y te voy a dar un numero aleatorio que es: {}".format(nombre1,aleatorio)
+print(cadenaFormat)
 
     
 
